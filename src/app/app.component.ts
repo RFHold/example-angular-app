5121ac0981f0fb1aa9287d388e2f50e371fa4cd1
Robert Holden <robert@robhold.com>
2021-09-03 12:31:58 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eaa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'example-angular-app';
+}
